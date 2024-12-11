e34b8c48ec98032925f179260b24b481197625b6
MikeJ28 <tungvshe176052@fpt.edu.vn>
2024-12-11 16:47:10 +0700

    Todo: storing data and hanlde logic

---
+++ enum.js
@@ -0,0 +1,23 @@
+//Create a enum message
+const Message = {
+    EXIT_SUCCESS: "Exiting...",
+    UNRECOGNIZED_COMMAND: "Unrecognized command",
+    WELCOME: "Welcome to the database shell. Type '.exit' to exit.",
+    HELP: "Help: Type '.help' to get help.",
+    INVALID_COMMAND: "Invalid command"
+}
+
+//Create a enum command
+const Command = {
+    EXIT: ".exit",
+    HELP: ".help",
+    CLEAR: ".clear",
+    SHOW: ".show",
+    CREATE: ".create",
+    INSERT: ".insert",
+    SELECT: ".select",
+    DELETE: ".delete",
+    UPDATE: ".update"
+}
+
+module.exports = { Message, Command };
--- main.js
+++ main.js
@@ -1,2 +1,61 @@
+const readlinePromis = require("node:readline/promises");
+const { Message, Command } = require("./enum.js");
 
-console.log("Hello World");
+const rl = readlinePromis.createInterface({ input: process.stdin, output: process.stdout });
+
+/**
+ * Create a prompt for the user to input a command
+ */
+function getUserPrompt() {
+    return rl.question("db > ");
+}
+
+/**
+ * This is logic handle the command
+ */
+function handleCommand(input) {
+    
+    switch(input) {
+        case Command.EXIT:
+            console.log(Message.EXIT_SUCCESS);
+            process.exit(0);
+        case Command.HELP:
+            console.log(Message.HELP);
+            break;
+        default:
+            console.log(Message.UNRECOGNIZED_COMMAND);
+            break;
+    }
+
+}
+
+/**
+ * This is the main function
+ */
+function main() {
+    //Welcome message:
+    console.log(Message.WELCOME);
+
+    function excecute() {
+        getUserPrompt().then(input => {
+            //Unrecognized command:
+            if(!input.startsWith('.')) {
+                console.log(Message.INVALID_COMMAND);
+                console.log(Message.HELP);
+            }
+            else { // handle the command:
+                handleCommand(input);
+            }
+
+            excecute();
+        }).catch(error => { // Exception handling:
+            console.log(error);
+            rl.close();
+        });
+    }
+
+    excecute();
+
+}
+
+main();
